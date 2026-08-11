6d40973a26eedaafe802ac90ed29f09831f9f91a
JDDP0708 <jddiazp@udisitrital.edu.co>
2026-08-11 12:28:19 -0500

    Vista implementacion, implementacion del abstractfactory

---
+++ src/co/edu/udistrital/view/AbstractInputOutputFactory.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package co.edu.udistrital.view;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public interface AbstractInputOutputFactory {
+    
+    public Input getInput();
+    
+    public Output getOutput();
+    
+}
